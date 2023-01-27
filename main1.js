b2d82c926493c4cd6af0a625c46d637e81afd813
William Pessoa <113470103+MatematicacomWillP@users.noreply.github.com>
2023-01-26 22:39:13 -0300

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,12 +1,12 @@
 
-var fim = new Date("jan 26, 2023 11:30:00").getTime();
+var fim = new Date("jan 26, 2023 11:30:00").getTime(); //colocando o tempo futuro
 var x = setInterval(function(){
 
 var agora = new Date().getTime();
-var diff = fim - agora;
-var dia = Math.floor(diff/(1000*60*60*24));
-var hora = Math.floor(diff %(1000*60*60*24)/(1000*60*60));
-var min = Math.floor(diff %(1000*60*60)/(1000*60));
+var diff = fim - agora; //para usar com o contador regrassivo
+var dia = Math.floor(diff/(1000*3600*24));
+var hora = Math.floor(diff %(1000*3600*24)/(1000*3600));
+var min = Math.floor(diff %(1000*3600)/(1000*60));
 var sec = Math.floor(diff %(1000*60)/1000);
 
 if (hora < 10) {
@@ -22,13 +22,13 @@ if (sec < 10) {
 
 
 myh1 = document.getElementById("container");
-myh1.innerHTML = `T- ${hora}:${min}:${sec}`
+myh1.innerHTML = `${hora}:${min}:${sec}`//contador regressivo
 
-if (diff < 0){
-    diff = agora - fim;
-    var dia = Math.floor(diff/(1000*60*60*24));
-    var hora = Math.floor(diff %(1000*60*60*24)/(1000*60*60));
-    var min = Math.floor(diff %(1000*60*60)/(1000*60));
+if (diff < 0){ //usando if para trocar de contador
+    diff = agora - fim; //para usar com o contador progressivo
+    var dia = Math.floor(diff/(1000*3600*24));
+    var hora = Math.floor(diff %(1000*3600*24)/(1000*3600));
+    var min = Math.floor(diff %(1000*3600)/(1000*60));
     var sec = Math.floor(diff %(1000*60)/1000);
     if (hora < 10) {
     hora = '0' + hora;
@@ -39,51 +39,7 @@ if (diff < 0){
     if (sec < 10) {
         sec = '0' + sec;
     }
-    myh1.innerHTML = `T+ ${hora}:${min}:${sec}`
+    myh1.innerHTML = `${hora}:${min}:${sec}` //contador progressivo
 }
 
 },1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const timeH = document.querySelector('h1');
-//let timeSecond = 600;//
-
-////timeH.innerHTML = `00:${timeSecond}`;//
-
-//const countDown =  setInterval (()=>{
-//    timeSecond--;
-//    displayTime(timeSecond);
-//    if (timeSecond <= 0 || timeSecond < 1){
-//        clearInterval(countDown)
-//    }
-//},1000)//
-
-//function displayTime(second){
-//    const min = Math.floor(second / 60);
-//    const sec = Math.floor(second % 60);
-//    timeH.innerHTML=`${min<10 ? '0': ''}${min}:${sec<10 ? '0': ''}${sec}`
-//}
